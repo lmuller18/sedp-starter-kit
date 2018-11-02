4577541c9d87dadf5e855b44a976ee3b0f33218e
Reint Jan Hoiting <reint-jan.hoiting@endurance.com>
2018-11-02 09:40:08 -0400

    Add Chat box and BoxCanvas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Grid from '@material-ui/core/Grid'
+import BoxCanvas from './BoxCanvas'
+import Chat from './Chat'
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Grid container>
+          <Grid item xs={9}>
+            <BoxCanvas />
+          </Grid>
+          <Grid item xs={3}>
+            <Chat />
+          </Grid>
+        </Grid>
       </div>
     );
   }
---
+++ frontend/src/Chat.js
@@ -0,0 +1,75 @@
+
+import React, {Component} from 'react'
+import {withStyles} from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import PropTypes from 'prop-types'
+import { Typography, TextField } from '@material-ui/core';
+
+const styles = {
+    boxStyle: {
+        width: 100,
+        height: 100,
+        backgroundColor: 'green',
+        position: 'absolute'
+    },
+    root: {
+        padding: 20
+    },
+    paper: {
+        width: '100%',
+        height: 600,
+        marginTop: 20,
+        position: 'relative',
+        paddingTop: 10
+    },
+    textField: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0
+    }
+}
+
+class Chat extends Component {
+    static propTypes=  {
+        classes: PropTypes.object
+    }
+    state = {
+        messages: ['Welcome to the SEDP program', 'Hello world', 'Foo bar']
+    }
+    onKeyDown = (event) => {
+        // event.keyCode = enter
+        if (event.keyCode === 13) {
+            const newMessages = [...this.state.messages]
+            newMessages.push(event.target.value)
+            this.setState({
+                messages: newMessages
+            })
+        } 
+        
+        if (event.keyCode === 8) {
+            console.log('delete')
+            if(!event.target.value || event.target.value === ''){
+                const newMessages = [...this.state.messages]
+                newMessages.splice(newMessages.length - 1, 1)
+                this.setState({
+                    messages: newMessages
+                })
+            }
+        }
+    }
+    render() {
+        return (<div className={this.props.classes.root}>
+            <Typography variant="display1">Canvas</Typography>
+            <Paper className={this.props.classes.paper}>
+                <div className={this.props.classes.content}>
+                    {this.state.messages && this.state.messages.map((message, index) => (<Typography key={index} variant='body1'>{message}</Typography>))}
+                </div>
+                <TextField className={this.props.classes.textField} onKeyDown={this.onKeyDown} variant='filled' label='message'/>
+            </Paper>
+        </div>
+        );
+    }
+}
+
+export default withStyles(styles)(Chat);
